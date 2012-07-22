1ccb6963bc671dc519851d23eac466e0256ea3ce
Nerian <siotopo@gmail.com>
2012-07-22 15:57:42 +0200

    update theme

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
   skip_before_filter :require_login
   
   def index
-    @posts = Post.all.order_by([:publish_date => :desc])
+    @posts = Post.all.order_by([[:publish_date, :desc]])
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,8 @@ module Blog
     config.assets.enabled = true
 
     config.assets.initialize_on_precompile = false
+    
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
     # Compile assets at run time
     config.assets.compile = true
